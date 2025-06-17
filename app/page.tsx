5d8530eb8f78dc6e5c59b323478878eb603d9a3b
Gerald Baalham <geraldbaalham@live.co.uk>
2025-06-17 16:52:50 +0100

    Implement CompanionCard, CompanionsList, and CTA components; enhance page layout with recent sessions and subject color functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,48 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div><h1>Hello</h1></div>
+    <main><h1>Popular Companions</h1>
+    <section className='home-section'>
+      <CompanionCard
+      id="123"
+      name="Neura the Brainy Explorer"
+      topic="Neural Network of the Brain"
+      subject="science"
+      duration = {45}
+      color="#ffda6e"
+      />
+      <CompanionCard
+      id="124"
+      name="Robo the Tech Guru"
+      topic="Artificial Intelligence"
+      subject="technology"
+      duration = {30}
+      color="#e5d0ff"
+      />
+      <CompanionCard
+      id="125"
+      name="Luna the Space Explorer"
+      topic="Astrophysics"
+      subject="science"
+      duration = {60}
+      color="#bde7ff"
+      />
+    </section>
+    
+    <section className='home-section'>
+      <CompanionsList
+      title="Recently Completed Sessions"
+      companions={recentSessions}
+      classNames="w-2/3 max-lg:w-full"
+      />
+      <CTA/>
+    </section>
+    </main>
   )
 }
 
